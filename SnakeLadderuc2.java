e8511a3d5cdb1425c98e89ee1d0370dd41850775
arshivam <shivam.armarkar007@gmail.com>
2022-01-20 12:07:33 +0530

    added random for die roll

---
+++ SnakeLadderuc2.java
@@ -0,0 +1,9 @@
+
+public class SnakeLadderuc2 {
+	public static void main(String[] agrs) {
+		int start = 0;
+		System.out.println("Position of player is "+start);
+		int roll = (int) (Math.floor(Math.random()*10) % 6+1);
+		System.out.println("Number on die is "+roll);
+	}
+}
