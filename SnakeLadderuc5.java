fcd81b4fb7aaac784bfe26f1c334f6e703886bd1
arshivam <shivam.armarkar007@gmail.com>
2022-01-20 14:44:53 +0530

    Ensure winning position is exack 100

--- SnakeLadderuc5.java
+++ SnakeLadderuc5.java
@@ -1,14 +1,13 @@
-
 public class SnakeLadderuc5 {
 	public static final int NO_PLAY = 0;
 	public static final int LADDER = 1;
 	public static final int SNAKE = 2;
-	public static final int START = 0;
+	public static final int start = 0;
 
 	public static void main(String[] agrs) {
 		int pos = 0;
-		System.out.println("Starting Position of player is "+START);
-		while (pos <= 100) {
+		System.out.println("Starting Position of player is "+start);
+		while (pos < 100) {
 			int roll = (int) (Math.floor(Math.random()*10) % 6 + 1);
 			System.out.println("Number on die is "+roll);
 			int condition = (int) (Math.floor(Math.random()*10)%3);
@@ -20,37 +19,31 @@ public class SnakeLadderuc5 {
 				break;
 			case LADDER:
 				pos = pos + roll;
-				if (pos <=100) {
-					System.out.println("New Position is: "+pos);
+				if (pos > 100) {
+					pos = pos - roll;
 				}
 				else {
-					pos = pos - roll;
-
-
 					System.out.println("New Position is: "+pos);
 				}
 				break;
 			case SNAKE:
 				pos = pos - roll;
-				if (pos <= 100) {
-					if (pos >= 0) {
-						System.out.println("New Position is: "+pos);
-					}
-					else {
-						pos = 0;
-						System.out.println("New Position is: "+pos);
-					}
-				}else {
+				if (pos >= 0) {
+					System.out.println("New Position is: "+pos);
+				}
+				else {
+					pos = 0;
 					System.out.println("New Position is: "+pos);
 				}
 				break;
 			default:
 				System.out.println("Default");
+				break;
 			}
 		}
 		System.out.println();
 		if (pos == 100) {
-			System.out.println("-----------");
+	        System.out.println("-----------");
 			System.out.println("PLAYER WON");
 			System.out.println("-----------");
 		}
