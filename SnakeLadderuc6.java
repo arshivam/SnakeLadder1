40a8633d5fd1f22ecadae91f984e2322fcff6f0e
arshivam <shivam.armarkar007@gmail.com>
2022-01-20 14:54:59 +0530

    added no of times dice rolled and position

--- SnakeLadderuc6.java
+++ SnakeLadderuc6.java
@@ -3,13 +3,14 @@ public class SnakeLadderuc6 {
 	public static final int NO_PLAY = 0;
 	public static final int LADDER = 1;
 	public static final int SNAKE = 2;
-	public static final int START = 0;
+	public static final int start = 0;
 
 	public static void main(String[] agrs) {
-		int pos = 0, count = 0; 
-		while (pos <= 100) {
+		int pos = 0, count = 0;
+		System.out.println("Starting Position of player is "+start);
+		while (pos < 100) {
 			int roll = (int) (Math.floor(Math.random()*10) % 6 + 1);
-			    count++;
+			count++;
 			int condition = (int) (Math.floor(Math.random()*10)%3);
 
 			switch (condition) {
@@ -18,37 +19,31 @@ public class SnakeLadderuc6 {
 				break;
 			case LADDER:
 				pos = pos + roll;
-				if (pos <=100) {
-					System.out.println("New Position is: "+pos);
+				if (pos > 100) {
+					pos = pos - roll;
 				}
 				else {
-					pos = pos - roll;
-
-
 					System.out.println("New Position is: "+pos);
 				}
 				break;
 			case SNAKE:
 				pos = pos - roll;
-				if (pos <= 100) {
-					if (pos >= 0) {
-						System.out.println("New Position is: "+pos);
-					}
-					else {
-						pos = 0;
-						System.out.println("New Position is: "+pos);
-					}
-				}else {
+				if (pos >= 0) {
+					System.out.println("New Position is: "+pos);
+				}
+				else {
+					pos = 0;
 					System.out.println("New Position is: "+pos);
 				}
 				break;
 			default:
 				System.out.println("Default");
+				break;
 			}
 		}
-		System.out.println();
-		System.out.println("No of time dice played : "+ count);
-		if (pos >= 100) {
+		System.out.println("No of time dice rolled is "+ count);
+		System.out.println("your current position is "+pos);
+		if (pos == 100) {
 			System.out.println("-----------");
 			System.out.println("PLAYER WON");
 			System.out.println("-----------");
